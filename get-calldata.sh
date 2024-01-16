150061e213492c693d40a037c93c4a3b533438f8
didi <git@d10r.net>
2024-01-16 14:30:26 +0100

    added function signature to output

--- get-calldata.sh
+++ get-calldata.sh
@@ -13,6 +13,7 @@ echo "OPERATION 1: approve USDCx SuperToken contract to transfer 300k USDC"
 # 300k with 6 decimals
 USDC_AMOUNT=300000000000
 echo "target: $USDC_ADDR"
+echo "function: approve(address spender, uint256 amount)"
 echo -n "calldata: "
 cast calldata "approve(address,uint256)" $USDCX_ADDR $USDC_AMOUNT
 echo
@@ -21,6 +22,7 @@ echo "OPERATION 2: wrap 300k USDC to USDCX"
 # 300k with 18 decimals
 USDCX_AMOUNT=$(cast to-wei 300000)
 echo "target $USDCX_ADDR"
+echo "function: upgrade(uint256 amount)"
 echo -n "calldata: "
 cast calldata "upgrade(uint256)" $USDCX_AMOUNT
 echo
@@ -30,8 +32,8 @@ echo "OPERATION 3: start flow to Safe with flowrate of 9,863.01369863 / day"
 FLOWRATE="114155251141550940"
 CFA_FWD_ADDR="0xcfA132E353cB4E398080B9700609bb008eceB125"
 echo "target: $CFA_FWD_ADDR"
+echo "function: setFlowrate(ISuperToken token, address receiver, int96 flowrate)"
 echo -n "calldata: "
-# setFlowrate(ISuperToken token, address receiver, int96 flowrate)
 cast calldata "setFlowrate(address,address,int96)" $USDCX_ADDR $STEWARDS_SAFE $FLOWRATE
 echo
 
@@ -41,12 +43,12 @@ AW_STRAT_ADDR="0x1D65c6d3AD39d454Ea8F682c49aE7744706eA96d"
 # 5.1M with 6 decimals
 AW_USDC_AMOUNT=5100000000000
 echo "target: $USDC_ADDR"
+echo "function: approve(address spender, uint256 amount)"
 echo -n "calldata: "
 cast calldata "approve(address,uint256)" $AW_STRAT_ADDR $AW_USDC_AMOUNT
 echo
 
 echo "OPERATION 5: create auto-wrap schedule"
-#function createWrapSchedule(address superToken, address strategy, address liquidityToken, uint64 expiry, uint64 lowerLimit,  uint64 upperLimit)
 AW_MGR_ADDR="0x30aE282CF477E2eF28B14d0125aCEAd57Fe1d7a1"
 # same expiry value as in https://github.com/superfluid-finance/widget/blob/master/packages/widget/src/CommandMapper.tsx#L102
 EXPIRY=3000000000
@@ -55,5 +57,6 @@ LOWER_LIMIT=1814400
 # 50 days (minimum is 7 days) - keeps the wrapped amount below 500k with monthly top-up
 UPPER_LIMIT=4320000
 echo "target: $AW_MGR_ADDR"
+echo "function: createWrapSchedule(address superToken, address strategy, address liquidityToken, uint64 expiry, uint64 lowerLimit,  uint64 upperLimit)"
 echo -n "calldata: "
 cast calldata "createWrapSchedule(address,address,address,uint64,uint64,uint64)" $USDCX_ADDR $AW_STRAT_ADDR $USDC_ADDR $EXPIRY $LOWER_LIMIT $UPPER_LIMIT
